7a28a23885c50b44ef5f18408df6a0c67ff7da07
SPT-Riyanka <riyanka.spt@gmail.com>
2024-06-14 12:37:17 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import StartGame from "./components/StartGame";
 
 function App() {
   return (
-    <>
-      {" "}
-      <div className="image"></div>
       <BrowserRouter>
         <Routes>
           <Route path="" element={<CreateRoom />} />
@@ -16,7 +13,6 @@ function App() {
           <Route path="/start" element={<StartGame />} />
         </Routes>
       </BrowserRouter>
-    </>
   );
 }
 
--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Form, FormGroup } from 'react-bootstrap'
 
 const CreateRoom = () => {
   return (
-    <div>CreateRoom</div>
+    <div className="image">
+      <FormGroup> 
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Jhon Deo" />
+      </Form.Group>
+      </FormGroup>
+    </div>
   )
 }
 
